2350d1f245c125a8e8c2fb40b25cfd1770e8b3ae
lambdadroid <lambdadroid@gmail.com>
2018-10-13 13:40:20 +0200

    ANDROID: Disable -Werror to fix build error on Android 9.0 (Pie)

--- src/Android.mk
+++ src/Android.mk
@@ -60,7 +60,7 @@ $(GEN): $(LOCAL_PATH)/intel_version.h.in $(wildcard $(LOCAL_PATH)/../.git/logs/H
 	sed -e "s|\@INTEL_DRIVER_GIT_VERSION\@|$$VER|" $< > $@
 LOCAL_GENERATED_SOURCES += $(GEN)
 
-LOCAL_CFLAGS := -DLINUX -g -Wall -Wno-unused -fvisibility=hidden
+LOCAL_CFLAGS := -DLINUX -g -Wall -Wno-error -Wno-unused -fvisibility=hidden
 
 LOCAL_SHARED_LIBRARIES := libdl libdrm libdrm_intel libcutils \
                libva libva-android libstdc++
